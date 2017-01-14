5778bb8c18371bccecdf3426ef4ee4a223b5fa41
H3mann <mann.harp@gmail.com>
2017-01-14 17:52:43 -0500

    buttons and checkout functionality working

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Buttons from '../src/Buttons'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <div>
-      test
-      </div>
+      
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
@@ -16,6 +15,10 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <div className="Buttons">
+        <Buttons />
+        </div>
       </div>
     );
   }
