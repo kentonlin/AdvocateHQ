cad1336d6d0876dff658987515eb5cd714f2ce1c
Kenton <kentonlin1@gmail.com>
2017-01-15 00:47:39 -0500

    width adjustment for contribute

--- src/App.js
+++ src/App.js
@@ -12,52 +12,54 @@ import kirsten from './Kirsten.png'
 class App extends Component {
   render() {
     return (
-      <div className="App">
-
-        <Navigate />
-        <div className="inlineContainerApp">
-          <img src={kirsten} style={{ borderRadius: "50%", width: '100px', height: '100px', display: 'inline'}} />
-          <div className="inlineApp">
-            <div>
-              Democrat for U.S. Senate
-            </div>
-            <div>
-              Kirsten Gillibrand
-            </div>
-            <div>
-              Senator, U.S. Senate New York
-            </div>
-            <div>
-              Incumbent
+      <div className="AppContainer">
+        
+        <div className="App">
+          <Navigate />
+          <div className="inlineContainerApp">
+            <img src={kirsten} style={{ borderRadius: "50%", width: '100px', height: '100px', display: 'inline'}} />
+            <div className="inlineApp">
+              <div>
+                Democrat for U.S. Senate
+              </div>
+              <div>
+                Kirsten Gillibrand
+              </div>
+              <div>
+                Senator, U.S. Senate New York
+              </div>
+              <div>
+                Incumbent
+              </div>
             </div>
           </div>
-        </div>
-        <Nav>
-          <NavGroup align="left">
-            <NavItem>
-              Overview
-            </NavItem>
-            <NavItem>
-              Support
-            </NavItem>
-            <NavItem>
-              Contribute
-            </NavItem>
-            <NavItem>
-              About
-            </NavItem>
-          </NavGroup>
-          <NavToggle />
-          <NavGroup align="right" isMenu>
-            <NavItem>
-              <Button>
-                Follow
-              </Button>
-            </NavItem>
-          </NavGroup>
-        </Nav>
+          <Nav>
+            <NavGroup align="left">
+              <NavItem>
+                Overview
+              </NavItem>
+              <NavItem>
+                Support
+              </NavItem>
+              <NavItem>
+                Contribute
+              </NavItem>
+              <NavItem>
+                About
+              </NavItem>
+            </NavGroup>
+            <NavToggle />
+            <NavGroup align="right" isMenu>
+              <NavItem>
+                <Button>
+                  Follow
+                </Button>
+              </NavItem>
+            </NavGroup>
+          </Nav>
 
-        <Contribute />
+          <Contribute />
+        </div>
       </div>
     );
   }
