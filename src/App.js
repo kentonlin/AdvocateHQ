23481fd6c67b6ad13ebfc762f981b0c9e17b8162
Kenton <kentonlin1@gmail.com>
2017-01-14 22:48:54 -0500

    before merge

--- src/App.js
+++ src/App.js
@@ -3,13 +3,32 @@ import logo from './logo.svg';
 import './App.css';
 import Contribute from './Contribute.js';
 import Navigate from './Nav.js';
+import { Image } from 're-bulma';
+import kirsten from './Kirsten.png'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <Navigate />
-      <Contribute />
+
+        <Navigate />
+          <img src={kirsten} style={{ borderRadius: "50%", width: '100px', height: '100px', display: 'inline'}} />
+          <div className="inline">
+
+          <div>
+            Democrat for U.S. Senate
+          </div>
+          <div>
+            Kirsten Gillibrand
+          </div>
+          <div>
+            Senator, U.S. Senate New York
+          </div>
+          <div>
+            Incumbent
+          </div>
+        </div>
+        <Contribute />
       </div>
     );
   }
